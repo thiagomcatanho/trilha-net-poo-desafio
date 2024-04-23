480a529a65f295b5c9e97fce929bfda902924b90
Thiago Catanho <thiagomcatanho@gmail.com>
2024-04-23 14:10:19 -0300

    desafio POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia("12369", "1100", "123456789", 16);
+nokia.Ligar();
+nokia.InstalarAplicativo("Jogo da Cobrinha");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone("12369", "12 pro max", "123456789",256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
